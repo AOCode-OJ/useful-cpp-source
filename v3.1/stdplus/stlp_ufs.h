507de6da64a33557eaf4b7e206e0432c5e44c620
CUICUI2010 <71164209+CUICUI2010@users.noreply.github.com>
2022-02-07 10:05:39 +0800

    Update stlp_ufs.h

--- v3.1/stdplus/stlp_ufs.h
+++ v3.1/stdplus/stlp_ufs.h
@@ -7,14 +7,17 @@
 namespace stdplus {
 
 	// Union-Find-Set declare here
+	// UPD: added weight
 	struct UFS {
-		std::vector<int> father;
+		std::vector<int> father, size_;
 
 		UFS() {
 			father = std::vector<int>();
+			size_ = std::vector<int>();
 		}
 		UFS(int n) {
 			father = std::vector<int>(n, -1);
+			size_ = std::vector<int>(n, 1);
 		}
 		int findFather(int x) {
 			if(father[x] < 0)
@@ -25,12 +28,24 @@ namespace stdplus {
 			int faA = findFather(a);
 			int faB = findFather(b);
 			if(faA != faB) {
-				father[faA] += father[faB];
+				father[faA] = -1;
 				father[faB] = faA;
+				size_[faA] += size_[faB];
+				size_[faB] = 0;
 			}
+		}
+		int findNum(){
+			set<int> s;
+			for(int i = 0;i < father.size();i++){
+				s.insert(FindFather(i));
+			}
+			return s.size();
 		}
 		int& operator[] (const int& pos) {
 			return father[pos];
+		}
+		int& operator() (const int& pos){
+			return size_[pos];
 		}
 	};
 	
