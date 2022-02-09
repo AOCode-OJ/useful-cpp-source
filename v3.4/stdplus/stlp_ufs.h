bff52b6c4d893c3ba68b99acac0fe2d8dbb11edc
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2022-02-09 10:41:45 +0800

    fixed serious bug in v3.4

--- v3.4/stdplus/stlp_ufs.h
+++ v3.4/stdplus/stlp_ufs.h
@@ -8,25 +8,25 @@ namespace stdplus {
 
 	// Union-Find-Set declare here
 	struct UFS {
-		std::vector<int> father;
+		std::vector<int> fa;
 
-		UFS() : father std::vector<int>() {}
-		UFS(int n) : father(std::vector<int>(n, -1)) {}
+		UFS() : fa(std::vector<int>()) {}
+		UFS(int n) : fa(std::vector<int>(n, -1)) {}
 		int findfa(int x) {
 			if(fa[x] < 0)
 				return x;
 			return fa[x] = findfa(fa[x]);
 		}
 		void unite(int a, int b) {
-			int faA = findFather(a);
-			int faB = findFather(b);
+			int faA = findfa(a);
+			int faB = findfa(b);
 			if(faA != faB) {
-				father[faA] += father[faB];
-				father[faB] = faA;
+				fa[faA] += fa[faB];
+				fa[faB] = faA;
 			}
 		}
 		int& operator[] (const int& pos) {
-			return father[pos];
+			return fa[pos];
 		}
 	};
 
